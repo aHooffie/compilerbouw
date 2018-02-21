c869acdabb7e7a53215c37bcc87da456663e49c8
61pancakes <andrea@Andreas-MacBook-Pro.local>
2018-02-21 21:18:36 +0100

    OCD print.c en print.h netjes gemaakt

--- 1/framework/src/print/print.h
+++ 1/framework/src/print/print.h
@@ -11,38 +11,43 @@
 
 #include "types.h"
 
-extern node *PRTstmts(node *arg_node, info *arg_info);
-extern node *PRTmodule(node *arg_node, info *arg_info);
-extern node *PRTassign(node *arg_node, info *arg_info);
-extern node *PRTvar(node *arg_node, info *arg_info);
-extern node *PRTvarlet(node *arg_node, info *arg_info);
-extern node *PRTbinop(node *arg_node, info *arg_info);
-extern node *PRTmonop(node *arg_node, info *arg_info);
-extern node *PRTfloat(node *arg_node, info *arg_info);
-extern node *PRTnum(node *arg_node, info *arg_info);
-extern node *PRTbool(node *arg_node, info *arg_info);
-extern node *PRTsymboltableentry(node *arg_node, info *arg_info);
-extern node *PRTerror(node *arg_node, info *arg_info);
+/* !! MODULE + DECLARATIONS AKA TOP BELOW!! */
 extern node *PRTdoPrint(node *syntaxtree);
-
-/* Add all missing core language print functions. */
-extern node *PRTif(node *arg_node, info *arg_info);
-extern node *PRTelse(node *arg_node, info *arg_info);
-extern node *PRTwhile(node *arg_node, info *arg_info);
-extern node *PRTfor(node *arg_node, info *arg_info);
-extern node *PRTreturn(node *arg_node, info *arg_info);
-extern node *PRTdowhile(node *arg_node, info *arg_info);
-
-/* Add function related prints. */
 extern node *PRTdeclarations(node *arg_node, info *arg_info);
-extern node *PRTparameters(node *arg_node, info *arg_info);
 extern node *PRTfunction(node *arg_node, info *arg_info);
+extern node *PRTparameters(node *arg_node, info *arg_info);
 extern node *PRTfunctionbody(node *arg_node, info *arg_info);
 extern node *PRTglobaldec(node *arg_node, info *arg_info);
 extern node *PRTglobaldef(node *arg_node, info *arg_info);
+extern node *PRTmodule(node *arg_node, info *arg_info);
+
+/* !! STATEMENTS BELOW !!*/
+extern node *PRTstmts(node *arg_node, info *arg_info);
+extern node *PRTassign(node *arg_node, info *arg_info);
+
+/* !! FUNCTION ELEMENTS BELOW !! */
+extern node *PRTif(node *arg_node, info *arg_info);
+extern node *PRTelse(node *arg_node, info *arg_info);
+extern node *PRTwhile(node *arg_node, info *arg_info);
+extern node *PRTreturn(node *arg_node, info *arg_info);
+extern node *PRTfor(node *arg_node, info *arg_info);
+extern node *PRTdowhile(node *arg_node, info *arg_info);
 extern node *PRTfunctioncallstmt(node *arg_node, info *arg_info);
-extern node *PRTfunctioncallexpr(node *arg_node, info *arg_info);
 extern node *PRTexpressions(node *arg_node, info *arg_info);
+
+/* !! EXPRESSIONS ARE BELOW !! */
+extern node *PRTbinop(node *arg_node, info *arg_info);
+extern node *PRTfloat(node *arg_node, info *arg_info);
+extern node *PRTnum(node *arg_node, info *arg_info);
+extern node *PRTbool(node *arg_node, info *arg_info);
+extern node *PRTvar(node *arg_node, info *arg_info);
+extern node *PRTvarlet(node *arg_node, info *arg_info);
+extern node *PRTmonop(node *arg_node, info *arg_info);
+extern node *PRTfunctioncallexpr(node *arg_node, info *arg_info);
 extern node *PRTcast(node *arg_node, info *arg_info);
+
+/* MISCELLANEOUS !! */
+extern node *PRTsymboltableentry(node *arg_node, info *arg_info);
+extern node *PRTerror(node *arg_node, info *arg_info);
 
 #endif /* _SAC_PRT_NODE_H_ */

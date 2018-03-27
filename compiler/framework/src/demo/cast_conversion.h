1edaf4374f5cb6632bd3c714b2424903235e36d9
61pancakes <andrea@Andreas-MacBook-Pro.local>
2018-03-27 19:30:12 +0200

    Begin milestone 9 & 10 gemaakt

---
+++ compiler/framework/src/demo/cast_conversion.h
@@ -0,0 +1,8 @@
+#ifndef _CAST_CONVERSION_H_
+#define _CAST_CONVERSION_H_
+#include "types.h"
+
+extern node *CCcast(node *arg_node, info *arg_info);
+extern node *CCdoCastConversion(node *syntaxtree);
+
+#endif

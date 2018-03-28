988adcf68436f1e75c3ea45148ec0ac564b999be
61pancakes <andrea@Andreas-MacBook-Pro.local>
2018-03-28 21:49:29 +0200

    Begin milestone 12

--- compiler/framework/src/codegen/gen_byte_code.c
+++ compiler/framework/src/codegen/gen_byte_code.c
@@ -5,18 +5,144 @@
 #include "traverse.h"
 #include "dbug.h"
 
+/* 
 
-/*
- * Traversal start function
- */
+    Milestone 12: Assembly Code Generation for Expressions and Statements
+    Implement a code generator that transforms your internal representation into a 
+    at sequence of CiviC-VM assembly instructions, pseudo instructions and labels. For this milestone leave out the
+    function call interface and restrict yourself to the body of the main function.
 
-node *GBCdoGenByteCode( node *syntaxtree)
+    Statements: ifelse for while dowhile return assign (functioncallstmt)
+    Expressions: ternop binop monop var num float cast bool (functioncallexpr arrayexpr)
+
+*/
+
+/* INFO structure */
+struct INFO
 {
-  DBUG_ENTER("GBCdoGenByteCode");
+};
 
-  /*
-   * Your work here....
-   */
+/* INFO structure macros */
 
-  DBUG_RETURN( syntaxtree);
+/* INFO functions */
+static info *MakeInfo(void)
+{
+    DBUG_ENTER("MakeInfo");
+
+    info *result;
+    result = (info *)MEMmalloc(sizeof(info));
+
+    DBUG_RETURN(result);
+}
+
+static info *FreeInfo(info *info)
+{
+    DBUG_ENTER("FreeInfo");
+
+    info = MEMfree(info);
+
+    DBUG_RETURN(info);
+}
+
+/* Statements */
+node *GBCifelse(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCifelse");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCfor(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCfor");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCwhile(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCwhile");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCdowhile(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCdowhile");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCreturn(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCreturn");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCassign(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCassign");
+    DBUG_RETURN(arg_node);
+}
+
+/* Expressions */
+node *GBCternop(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCternop");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCbinop(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCbinop");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCmonop(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCmonop");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCvar(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCvar");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCnum(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCnum");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCfloat(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCfloat");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCbool(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCbool");
+    DBUG_RETURN(arg_node);
+}
+
+node *GBCcast(node *arg_node, info *arg_info)
+{
+    DBUG_ENTER("GBCcast");
+    DBUG_RETURN(arg_node);
+}
+
+/* Traversal start function */
+node *GBCdoGenByteCode(node *syntaxtree)
+{
+    DBUG_ENTER("GBCdoGenByteCode");
+
+    info *arg_info;
+    arg_info = MakeInfo();
+
+    TRAVpush(TR_gbc);
+    syntaxtree = TRAVdo(syntaxtree, arg_info);
+    TRAVpop();
+
+    arg_info = FreeInfo(arg_info);
+
+    DBUG_RETURN(syntaxtree);
 }
--- compiler/framework/src/codegen/gen_byte_code.h
+++ compiler/framework/src/codegen/gen_byte_code.h
@@ -2,6 +2,24 @@
 #define _GEN_BYTE_CODE_H_
 #include "types.h"
 
-extern node *GBCdoGenByteCode( node *syntaxtree);
+/* Statements*/
+extern node *GBCifelse(node *arg_node, info *arg_info);
+extern node *GBCfor(node *arg_node, info *arg_info);
+extern node *GBCwhile(node *arg_node, info *arg_info);
+extern node *GBCdowhile(node *arg_node, info *arg_info);
+extern node *GBCreturn(node *arg_node, info *arg_info);
+extern node *GBCassign(node *arg_node, info *arg_info);
+
+/* Expressions */
+extern node *GBCternop(node *arg_node, info *arg_info);
+extern node *GBCbinop(node *arg_node, info *arg_info);
+extern node *GBCmonop(node *arg_node, info *arg_info);
+extern node *GBCvar(node *arg_node, info *arg_info);
+extern node *GBCnum(node *arg_node, info *arg_info);
+extern node *GBCfloat(node *arg_node, info *arg_info);
+extern node *GBCcast(node *arg_node, info *arg_info);
+extern node *GBCbool(node *arg_node, info *arg_info);
+
+extern node *GBCdoGenByteCode(node *syntaxtree);
 
 #endif

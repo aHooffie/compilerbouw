cf9711c11c4323c3f9dc622c50d43cdf214d0072
Aynel <aynell__@hotmail.com>
2018-03-30 16:16:38 +0200

    opzet expressions gen

--- compiler/framework/src/codegen/gen_3.c
+++ compiler/framework/src/codegen/gen_3.c
@@ -9,19 +9,58 @@ node *GBCbinop(node *arg_node, info *arg_info)
 {
     DBUG_ENTER("GBCbinop");
 
+    node *n;
+
+    // traverse expression
+    BINOP_LEFT(arg_node) = TRAVdo(BINOP_LEFT(arg_node), arg_info);
+    nodetype left = NODE_TYPE(BINOP_LEFT(arg_node));
+
     // find out what binop
     switch(BINOP_OP(arg_node))
     {
         case BO_add:
-            // code
+            if (left == N_num)
+            {
+                n = TBmakeInstructions(I_iadd, NULL);
+            }
+            else
+            {
+                n = TBmakeInstructions(I_fadd, NULL);
+            }
 
         case BO_sub:
+            if (left == N_num)
+            {
+                n = TBmakeInstructions(I_isub, NULL);
+            }
+            else
+            {
+                n = TBmakeInstructions(I_fsub, NULL);
+            }
 
         case BO_mul:
+            if (left == N_num)
+            {
+                n = TBmakeInstructions(I_imul, NULL);
+            }
+            else
+            {
+                n = TBmakeInstructions(I_fmul, NULL);
+            }
 
         case BO_div:
+            if (left == N_num)
+            {
+                n = TBmakeInstructions(I_idiv, NULL);
+            }
+            else
+            {
+                n = TBmakeInstructions(I_fdiv, NULL);
+            }
 
         case BO_mod:
+            // (only for integers)
+            n = TBmakeInstructions(I_irem, NULL);
 
         case BO_lt:
 
@@ -32,18 +71,45 @@ node *GBCbinop(node *arg_node, info *arg_info)
         case BO_ge:
 
         case BO_eq:
+        // equal
+            if (left == N_num)
+            {
+                n = TBmakeInstructions(I_ieq, NULL);
+            }
+            else if (left == N_FLOAT)
+            {
+                n = TBmakeInstructions(I_feq, NULL);
+            }
+            else // bool
+            {
+                n = TBmakeInstructions(I_beq, NULL);
+            }
 
         case BO_ne:
+        // not equal
+            if (left == N_num)
+            {
+                n = TBmakeInstructions(I_ine, NULL);
+            }
+            else if (left == N_FLOAT)
+            {
+                n = TBmakeInstructions(I_fne, NULL);
+            }
+            else // bool
+            {
+                n = TBmakeInstructions(I_bne, NULL);
+            }
 
         case BO_and:
 
         case BO_or:
 
-        case BO_unknown:
+        // case BO_unknown:
 
-        default:
-            // code
     }
+
+    /* Add the node to the list of instructions. */
+    addNode(n, arg_info);
 
     DBUG_RETURN(arg_node);
 }

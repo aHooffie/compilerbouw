ade9458a254cc00ea06cfc3e2f4358eab40fd353
Aynel <aynell__@hotmail.com>
2018-03-30 16:52:45 +0200

    opzet expressions gen

--- compiler/framework/src/codegen/gen_3.c
+++ compiler/framework/src/codegen/gen_3.c
@@ -13,6 +13,8 @@ node *GBCbinop(node *arg_node, info *arg_info)
 
     // traverse expression
     BINOP_LEFT(arg_node) = TRAVdo(BINOP_LEFT(arg_node), arg_info);
+    BINOP_RIGHT(arg_node) = TRAVdo(BINOP_RIGHT(arg_node), arg_info);
+
     nodetype left = NODE_TYPE(BINOP_LEFT(arg_node));
 
     // find out what binop
@@ -59,19 +61,49 @@ node *GBCbinop(node *arg_node, info *arg_info)
             }
 
         case BO_mod:
-            // (only for integers)
             n = TBmakeInstructions(I_irem, NULL);
 
         case BO_lt:
+            if (left == N_num)
+            {
+                n = TBmakeInstructions(I_ilt, NULL);
+            }
+            else
+            {
+                n = TBmakeInstructions(I_flt, NULL);
+            }
 
         case BO_le:
+            if (left == N_num)
+            {
+                n = TBmakeInstructions(I_ile, NULL);
+            }
+            else
+            {
+                n = TBmakeInstructions(I_fle, NULL);
+            }
 
         case BO_gt:
+            if (left == N_num)
+            {
+                n = TBmakeInstructions(I_igt, NULL);
+            }
+            else
+            {
+                n = TBmakeInstructions(I_fgt, NULL);
+            }
 
         case BO_ge:
+            if (left == N_num)
+            {
+                n = TBmakeInstructions(I_ige, NULL);
+            }
+            else
+            {
+                n = TBmakeInstructions(I_fge, NULL);
+            }
 
         case BO_eq:
-        // equal
             if (left == N_num)
             {
                 n = TBmakeInstructions(I_ieq, NULL);
@@ -86,7 +118,6 @@ node *GBCbinop(node *arg_node, info *arg_info)
             }
 
         case BO_ne:
-        // not equal
             if (left == N_num)
             {
                 n = TBmakeInstructions(I_ine, NULL);
@@ -104,8 +135,6 @@ node *GBCbinop(node *arg_node, info *arg_info)
 
         case BO_or:
 
-        // case BO_unknown:
-
     }
 
     /* Add the node to the list of instructions. */
@@ -117,6 +146,26 @@ node *GBCbinop(node *arg_node, info *arg_info)
 node *GBCmonop(node *arg_node, info *arg_info)
 {
     DBUG_ENTER("GBCmonop");
+
+    // traverse expression
+    MONOP_EXPR(arg_node) = TRAVdo(MONOP_EXPR(arg_node), arg_info);
+    nodetype type = NODE_TYPE(MONOP_EXPR(arg_node));
+
+    switch (MONOP_OP(arg_node))
+    {
+        case MO_neg:
+            if (type == N_num)
+                n = TBmakeInstructions(I_ineg, NULL);
+            else
+                n = TBmakeInstructions(I_fneg, NULL);
+
+        case MO_not:
+            n = TBmakeInstructions(I_bnot, NULL);
+    }
+
+    /* Add the node to the list of instructions. */
+    addNode(n, arg_info);
+
     DBUG_RETURN(arg_node);
 }
 

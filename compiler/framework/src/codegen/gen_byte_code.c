5309c94a99cfc3a96e462da898f058e6a0c4d4dc
Aynel <aynell__@hotmail.com>
2018-03-31 16:23:12 +0200

    onopgeloste bugs code gen/print

--- compiler/framework/src/codegen/gen_byte_code.c
+++ compiler/framework/src/codegen/gen_byte_code.c
@@ -84,6 +84,7 @@ node *GBCternop(node *arg_node, info *arg_info)
     {
 
         // !!!!! KLOPT DIT? NALOPEN
+        // ADD AND/OR
         case BO_add:
             n = TBmakeInstructions(I_badd, NULL);
             break;
@@ -113,12 +114,14 @@ node *GBCbinop(node *arg_node, info *arg_info)
 
     nodetype left = NODE_TYPE(BINOP_LEFT(arg_node));
 
-    // find out what binop
+    printf("node line: %i\n", NODE_LINE(arg_node));
+
+    // find out binop type
     switch(BINOP_OP(arg_node))
     {
         case BO_add:
             if (left == N_num)
-                // optimize : met iinc
+                // >>> optimize : met iinc??
                 n = TBmakeInstructions(I_iadd, NULL);
             else
                 n = TBmakeInstructions(I_fadd, NULL);
@@ -296,6 +299,8 @@ node *GBCnum(node *arg_node, info *arg_info)
     DBUG_ENTER("GBCnum");
     node *n;
 
+    CTInote("in num!\n");
+
     /* If the value of the integer is either 0 or 1, create a basic instruction, otherwise a custom instruction. */
     if (NUM_VALUE(arg_node) == 0)
         n = TBmakeInstructions(I_iloadc_0, NULL);
@@ -311,6 +316,7 @@ node *GBCnum(node *arg_node, info *arg_info)
     }
 
     /* Add the node to the list of instructions. */
+    CTInote("adding node\n");
     addNode(n, arg_info);
     DBUG_RETURN(arg_node);
 }
@@ -378,11 +384,357 @@ node *GBCdoGenByteCode(node *syntaxtree)
     info *arg_info;
     arg_info = MakeInfo();
 
-    TRAVpush(TR_as);
+    TRAVpush(TR_gbc);
     syntaxtree = TRAVdo(syntaxtree, arg_info);
     TRAVpop();
+
+    printInstructions(arg_info);
 
     arg_info = FreeInfo(arg_info);
 
     DBUG_RETURN(syntaxtree);
+}
+
+
+/* Helper functions */
+char *instrToString(instr type)
+{
+    char *s = NULL;
+    switch (type)
+    {
+    case I_iadd:
+        s = "iadd";
+        break;
+    case I_fadd: { s = "fadd"; break; }
+    case I_isub:
+        s = "isub";
+        break;
+    case I_fsub:
+        s = "fsub";
+        break;
+    case I_imul:
+        s = "imul";
+        break;
+    case I_fmul:
+        s = "fmul";
+        break;
+    case I_idiv:
+        s = "idiv";
+        break;
+    case I_fdiv:
+        s = "fdiv";
+        break;
+    case I_irem:
+        s = "irem";
+        break;
+    case I_ineg:
+        s = "ineg";
+        break;
+    case I_fneg:
+        s = "fneg";
+        break;
+    case I_bnot:
+        s = "bnot";
+        break;
+    case I_iinc:
+        s = "iinc";
+        break;
+    case I_iinc_1:
+        s = "iinc_1";
+        break;
+    case I_idec:
+        s = "idec";
+        break;
+    case I_idec_1:
+        s = "idec_1";
+        break;
+    case I_badd:
+        s = "badd";
+        break;
+    case I_bmul:
+        s = "bmul";
+        break;
+    case I_ine:
+        s = "ine";
+        break;
+    case I_ieq:
+        s = "ieq";
+        break;
+    case I_ilt:
+        s = "ilt";
+        break;
+    case I_ile:
+        s = "ile";
+        break;
+    case I_igt:
+        s = "igt";
+        break;
+    case I_ige:
+        s = "ige";
+        break;
+    case I_fne:
+        s = "fne";
+        break;
+    case I_feq:
+        s = "feq";
+        break;
+    case I_flt:
+        s = "flt";
+        break;
+    case I_fle:
+        s = "fle";
+        break;
+    case I_fgt:
+        s = "fgt";
+        break;
+    case I_fge:
+        s = "fge";
+        break;
+    case I_bne:
+        s = "bne";
+        break;
+    case I_beq:
+        s = "beq";
+        break;
+    case I_isr:
+        s = "isr";
+        break;
+    case I_isrn:
+        s = "isrn";
+        break;
+    case I_isrl:
+        s = "isrl";
+        break;
+    case I_isrg:
+        s = "isrg";
+        break;
+    case I_jsr:
+        s = "jsr";
+        break;
+    case I_jsre:
+        s = "jsre";
+        break;
+    case I_esr:
+        s = "esr";
+        break;
+    case I_ireturn:
+        s = "ireturn";
+        break;
+    case I_freturn:
+        s = "freturn";
+        break;
+    case I_breturn:
+        s = "breturn";
+        break;
+    case I_return:
+        s = "return";
+        break;
+    case I_jump:
+        s = "jump";
+        break;
+    case I_branch_t:
+        s = "branch_t";
+        break;
+    case I_branch_f:
+        s = "branch_f";
+        break;
+    case I_iload:
+        s = "iload";
+        break;
+    case I_iload_0:
+        s = "iload_0";
+        break;
+    case I_iload_1:
+        s = "iload_1";
+        break;
+    case I_iload_2:
+        s = "iload_2";
+        break;
+    case I_iload_3:
+        s = "iload_3";
+        break;
+    case I_fload:
+        s = "fload";
+        break;
+    case I_fload_0:
+        s = "fload_0";
+        break;
+    case I_fload_1:
+        s = "fload_1";
+        break;
+    case I_fload_2:
+        s = "fload_2";
+        break;
+    case I_fload_3:
+        s = "fload_3";
+        break;
+    case I_bload:
+        s = "bload";
+        break;
+    case I_bload_0:
+        s = "bload_0";
+        break;
+    case I_bload_1:
+        s = "bload_1";
+        break;
+    case I_bload_2:
+        s = "bload_2";
+        break;
+    case I_bload_3:
+        s = "bload_3";
+        break;
+    case I_iloadn:
+        s = "iloadn";
+        break;
+    case I_floadn:
+        s = "floadn";
+        break;
+    case I_bloadn:
+        s = "bloadn";
+        break;
+    case I_iloadg:
+        s = "iloadg";
+        break;
+    case I_floadg:
+        s = "floadg";
+        break;
+    case I_bloadg:
+        s = "bloadg";
+        break;
+    case I_iloade:
+        s = "iloade";
+        break;
+    case I_floade:
+        s = "floade";
+        break;
+    case I_bloade:
+        s = "bloade";
+        break;
+    case I_iloadc:
+        s = "iloadc";
+        break;
+    case I_floadc:
+        s = "floadc";
+        break;
+    case I_bloadc:
+        s = "bloadc";
+        break;
+    case I_iloadc_0:
+        s = "iloadc_0";
+        break;
+    case I_floadc_0:
+        s = "floadc_0";
+        break;
+    case I_bloadc_t:
+        s = "bloadc_t";
+        break;
+    case I_iloadc_1:
+        s = "iloadc_1";
+        break;
+    case I_floadc_1:
+        s = "floadc_1";
+        break;
+    case I_bloadc_f:
+        s = "bloadc_f";
+        break;
+    case I_iloadc_m1:
+        s = "iloadc_m1";
+        break;
+    case I_istore:
+        s = "istore";
+        break;
+    case I_fstore:
+        s = "fstore";
+        break;
+    case I_bstore:
+        s = "bstore";
+        break;
+    case I_istoren:
+        s = "istoren";
+        break;
+    case I_fstoren:
+        s = "fstoren";
+        break;
+    case I_bstoren:
+        s = "bstoren";
+        break;
+    case I_istoreg:
+        s = "istoreg";
+        break;
+    case I_fstoreg:
+        s = "fstoreg";
+        break;
+    case I_bstoreg:
+        s = "bstoreg";
+        break;
+    case I_istoree:
+        s = "istoree";
+        break;
+    case I_fstoree:
+        s = "fstoree";
+        break;
+    case I_bstoree:
+        s = "bstoree";
+        break;
+    case I_i2f:
+        s = "i2f";
+        break;
+    case I_f2i:
+        s = "f2i";
+        break;
+    case I_ipop:
+        s = "ipop";
+        break;
+    case I_fpop:
+        s = "fpop";
+        break;
+    case I_bpop:
+        s = "bpop";
+        break;
+    case I_ownbranch:
+        s = "";
+    case I_unknown:
+        CTIabort("Unknown instruction type.");
+        break;
+    default:
+        CTIabort("Unknown instruction type.");
+        break;
+    }
+    return s;
+}
+
+
+void printInstructions(info *arg_info)
+{
+    node *n = INFO_FI(arg_info);
+
+    // TO DO: INDENTATIE + LABELS WITH :
+
+    /* Print all instructions */
+    while (n != NULL)
+    {
+        printf("%s", instrToString(INSTRUCTIONS_INSTR(n)));
+
+        // if (INSTRUCTIONS_OFFSET(n) != NULL)
+            printf(" %i", INSTRUCTIONS_OFFSET(n));
+
+        // if (INSTRUCTIONS_ARG(n) != NULL)
+            printf(" %s", INSTRUCTIONS_ARG(n));
+
+        printf("\n");
+
+        n = INSTRUCTIONS_NEXT(n);
+    }
+
+    /* Print the last instruction. */
+    printf("%s", instrToString(INSTRUCTIONS_INSTR(n)));
+    // if (INSTRUCTIONS_OFFSET(n) != NULL)
+        printf(" %i", INSTRUCTIONS_OFFSET(n));
+
+    // if (INSTRUCTIONS_ARG(n) != NULL)
+        printf(" %s", INSTRUCTIONS_ARG(n));
+
+    printf("\n");
+
+    /* Print mogelijk nog andere instructies met een . (voor later) */
 }
